e2c85d011e1ae6b1b85294826e9445a861fcaf9f
elfi <alrigaff@gmail.com>
2021-03-12 17:58:11 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="wrapper">
+    <div class="wrapper-content">
+      <section>
+        <div class="container">
+          <button
+            class="btn btnPrimary"
+            @click="modalFirst =  !modalFirst">Show first modal</button>
+          <!--First model-->
+          <models
+            title="First model"
+            v-if="modalFirst"
+            @close="modalFirst =  !modalFirst"></models>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+import models from '@/components/Modal.vue'
+
+export default {
+  components: {
+    models
+  },
+  data() {
+    return {
+      modalFirst: false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
