689dc5e0afe320db912c5ddc8e2aea6a9ebfd5bd
elfi <alrigaff@gmail.com>
2021-03-18 15:50:16 +0300

    fixed submit function

--- src/components/ModalValidate.vue
+++ src/components/ModalValidate.vue
@@ -79,6 +79,12 @@ export default {
           email: this.email,
         }
         console.log(user)
+
+        //done
+        this.name = ''
+        this.email = ''
+        this.$v.$reset()
+        this.$emit('close')
       }
     }
   }
